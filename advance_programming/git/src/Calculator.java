53cdb4cb30386106e96df0cf0c936d305d78d593
Moein Arabi <moein.mo81@gmail.com>
2023-10-02 17:37:32 +0330

    Fix the facto methos

--- advance_programming/git/src/Calculator.java
+++ advance_programming/git/src/Calculator.java
@@ -26,7 +26,7 @@ public class Calculator {
     public static int factorial(int a) {
         int result = 1;
         for (int i = 2; i <= a; i++) {
-            result *= i;
+            result += i;
         }
         return result;
     }
